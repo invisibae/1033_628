c36dd7f99040276b3a4d2b3cad6bf12290a032aa
Greg Morton <gregorymorton96@gmail.com>
2022-03-09 17:18:55 -0500

    created basic table

---
+++ app.py
@@ -0,0 +1,42 @@
+import csv
+from flask import Flask
+from flask import abort
+from flask import render_template
+app = Flask(__name__)
+
+def get_csv_big():
+    csv_path = './static/1033_clean_2_24.csv'
+    csv_file = open(csv_path, 'r')
+    csv_obj = csv.DictReader(csv_file)
+    csv_list = list(csv_obj)
+    return csv_list
+
+def get_csv_summary():
+    csv_path = './static/1033_clean_2_24.csv'
+    csv_file = open(csv_path, 'r')
+    csv_obj = csv.DictReader(csv_file)
+    csv_list = list(csv_obj)
+    return csv_list
+
+
+
+@app.route("/")
+def index():
+    template = 'index.html'
+    object_list = get_csv_big()
+    return render_template(template, object_list=object_list)
+
+@app.route('/<row_id>/')
+def detail(row_id):
+    template = 'detail.html'
+    object_list = get_csv_big()
+    for row in object_list:
+        if row['id'] == row_id:
+            return render_template(template, object=row)
+    abort(404)
+
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True, use_reloader=True)
--- md_1033.R
+++ md_1033.R
@@ -3,6 +3,8 @@ library(dplyr)
 library(tidyr)
 library(stringr)
 library(lubridate)
+library(linelist)
+library(kml_to_csv)
 
 
 # Load Data ---------------------------------------------------------------
@@ -21,17 +23,57 @@ md_1033 <- md_1033 %>%
       str_to_lower(.) %>%
       str_replace_all(., ' ', '_'))) 
 
+# Let's make acquisition value numeric 
+
+md_1033$acquisition_value <- md_1033$acquisition_value %>% 
+  str_replace(., "\\$", "") %>%
+  str_replace(., "\\,", "") %>%
+  as.numeric() 
+  
+
 
 # Next, convert ship date to date time
 
-md_1033$ship_date %>% as_datetime('%m/%d/%y/%T')
+md_1033$ship_date <- md_1033$ship_date %>%
+  str_replace(.,"AM", "") %>%
+  mdy_hms()
 
+
+# Convert to csv
+write_csv(md_1033, "1033_clean_2_24.csv")
+
+# Make Function to Calculate Mode  ----------------------------------------
+
+calculate_mode <- function(x) {
+  uniqx <- unique(na.omit(x))
+  uniqx[which.max(tabulate(match(x, uniqx)))]
+}
+
+
+
+write_csv(md_1033, "1033_clean_2_24.csv")
+
+summary_table <- md_1033 %>%
+  group_by(agency_name) %>%
+  summarize(total_items = sum(quantity),
+            total_acquisition_value = sum(quantity * acquisition_value),
+            most_acquired_item = calculate_mode(item_name)
+            ) %>%
+  mutate(agency_name_2 = paste0(agency_name, ", Maryland")) %>%
+  arrange(desc(total_acquisition_value))
+
+write_csv(summary_table, "summary_table.csv")
 
 
 
 
 md_1033 %>%
-  group_by(Item Name) %>%
-  summarize(count = n()) %>%
-  arrange(desc(count))
+  filter(agency_name == "PRINCE GEORGE COUNTY POLICE DEPT") %>%
+  group_by(item_name) %>%
+  summarize(count = n())
+
+
+
+
+
 
