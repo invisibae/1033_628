eb18d55ff9f8a76aff5c406c0b76eacac4760302
Greg Morton <gregorymorton96@gmail.com>
2022-03-13 20:38:52 -0400

    We did it

--- freeze.py
+++ freeze.py
@@ -1,11 +1,11 @@
 from flask_frozen import Freezer
-from app import app, get_csv
+from app import app, get_csv_summary
 freezer = Freezer(app)
 
 @freezer.register_generator
 def detail():
-    for row in get_csv():
-        yield {'row_id': row['id']}
+    for row in get_csv_summary():
+        yield {'slug': row['slug']}
 
 if __name__ == '__main__':
     freezer.freeze()
